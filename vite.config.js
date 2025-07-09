329237e28b343f18af806ceb684e28102a326110
alexandergostajob <alexander.gosta@ri.se>
2025-07-09 09:52:30 +0200

    updated site links

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/DesignTech_graph/",
+  base: "/designtech-graph/",
   plugins: [
     react(),
     tailwindcss(),
